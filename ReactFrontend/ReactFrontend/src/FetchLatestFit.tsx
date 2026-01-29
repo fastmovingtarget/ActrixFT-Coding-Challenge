92d010c9c4cf52f53805a07f383cf6461f8413fa
Target <fastmovingtarget220689@gmail.com>
2026-01-29 21:50:05 +0000

    Fetch latest fit data to show a graph visual

---
+++ ReactFrontend/ReactFrontend/src/FetchLatestFit.tsx
@@ -0,0 +1,28 @@
+import {type LatestFit } from "./Types/Types"
+
+export const fetchLatestFit = async (
+    country : 'US' | 'UK',
+    setLatestFit : React.Dispatch<React.SetStateAction<LatestFit | null>>
+) => {
+
+    const returnPromise = new Promise<'Success' | 'Failure'>((resolve) => {
+        console.log("Getting Data")
+        fetch(
+            `http://localhost:5000/latestfit?country=${country}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            }
+        ).then((rawData) => {
+            rawData.json().then((data : LatestFit) => {
+                data.Constants = data.Constants.map((constant) => Number(constant))
+
+                setLatestFit(data)
+                resolve("Success")
+            })
+        })
+    })
+    return returnPromise;
+}
---
+++ ReactFrontend/ReactFrontend/src/LatestFitGraph.tsx
@@ -0,0 +1,76 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { type LatestFit } from './Types/Types';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
+export default function LatestFitGraph ({latestFit} : {latestFit : LatestFit}) {
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top' as const,
+                display: false
+            },
+            title: {
+                display: true,
+                text: 'Maturity Fit Curve',
+            },
+        },
+    };
+
+    const findYield = (maturity : number) => {
+        if(latestFit.Curve === "ns"){
+            return (
+                latestFit.Constants[0] + 
+                latestFit.Constants[1] * ((1 - Math.exp(-maturity / latestFit.Constants[3])) / (maturity / latestFit.Constants[3])) + 
+                latestFit.Constants[2] * (((1 - Math.exp(-maturity / latestFit.Constants[3])) / (maturity / latestFit.Constants[3])) - Math.exp(-maturity / latestFit.Constants[3]))
+            )
+        }
+        else
+            return (
+                latestFit.Constants[0]*maturity + latestFit.Constants[1]
+            )
+    }
+
+    const dataset = new Array(600).fill(0).map((element, index) => {
+        const maturity = (index + 1)/12;
+        const m_yield = findYield(maturity)
+        return [maturity.toFixed(2), m_yield]
+    })
+
+    const data = {
+        labels: [0,5,10,15,20,25,30,35,40,45,50,55,60],
+        datasets: [
+            {
+                data: dataset,
+                borderColor: 'rgb(99, 132, 255)',
+            }
+        ],
+    };
+
+
+
+    return <>
+        <Line options={options} data={data}/>
+    </>
+}
