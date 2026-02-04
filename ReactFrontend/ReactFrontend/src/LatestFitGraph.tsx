79032d246802762339e1c31434e1900e9fbb0cb0
Target <fastmovingtarget220689@gmail.com>
2026-02-04 10:20:31 +0000

    Latest fit graph shows 2 scatters, fit and existing points

--- ReactFrontend/ReactFrontend/src/LatestFitGraph.tsx
+++ ReactFrontend/ReactFrontend/src/LatestFitGraph.tsx
@@ -8,7 +8,7 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
-import { Line } from 'react-chartjs-2';
+import { Chart } from 'react-chartjs-2';
 import { type LatestFit } from './Types/Types';
 
 ChartJS.register(
@@ -36,6 +36,9 @@ export default function LatestFitGraph ({latestFit} : {latestFit : LatestFit}) {
                 text: 'Maturity Fit Curve',
             },
         },
+        interaction:{
+            mode:"nearest",
+        }
     };
 
     const findYield = (maturity : number) => {
@@ -52,6 +55,10 @@ export default function LatestFitGraph ({latestFit} : {latestFit : LatestFit}) {
             )
     }
 
+    const scatterData = latestFit.MaturityData.map((maturity, index) => {
+        return [maturity, latestFit.YieldData[index]]
+    })
+
     const dataset = new Array(600).fill(0).map((_, index) => {
         const maturity = (index + 1)/12;
         const m_yield = findYield(maturity)
@@ -59,18 +66,25 @@ export default function LatestFitGraph ({latestFit} : {latestFit : LatestFit}) {
     })
 
     const data = {
-        labels: [0,5,10,15,20,25,30,35,40,45,50,55,60],
+        labels: [0, 60],
         datasets: [
             {
                 data: dataset,
                 borderColor: 'rgb(99, 132, 255)',
+                pointRadius: 0.5,
+                order:2
+            },
+            {
+                data:scatterData,
+                backgroundColor: 'rgb(255, 132, 99)',
+                order:1
             }
         ],
     };
 
 
-
+//technically we're drawint 2 scatter plots, but the scatter points of the fit curve are so close together they're indistinguishable froma  line
     return <>
-        <Line options={options} data={data}/>
+        <Chart type="scatter" options={options} data={data}/>
     </>
 }
